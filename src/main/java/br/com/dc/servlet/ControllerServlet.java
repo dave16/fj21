646a2b961b91f7b6e815040b9d3ec2ad37e7a916
dave16 <batistabtd@gmail.com>
2015-02-13 00:08:50 -0200

    Primeira logica mvc

---
+++ src/main/java/br/com/dc/servlet/ControllerServlet.java
@@ -0,0 +1,33 @@
+package br.com.dc.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.com.dc.logica.Logica;
+
+public class ControllerServlet extends HttpServlet{
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		 
+		String parametro = request.getParameter("logica");
+		String nomeDaClasse = "br.com.dc.logica." + parametro;
+		
+		try{
+			Class classe = Class.forName(nomeDaClasse);
+			
+			Logica logica = (Logica) classe.newInstance();
+			logica.executa(request, response);
+			
+		}catch(Exception e){
+			throw new ServletException("A logica de negócios causou uma exceção" + e);
+		}
+		
+		
+	}
+}
